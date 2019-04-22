50042cb63b32448cea15c887358714acbf3197ee
Gurikov Ilya <ilion9871@gmail.com>
2019-04-22 17:09:26 +0300

    ui added snake

--- js/iphone/apps/snake/snake.js
+++ js/iphone/apps/snake/snake.js
@@ -7,13 +7,60 @@ class Snake extends Application
 
         // Сюда вставляем html приложения
         this.component = `
-            <img src="http://www.iconarchive.com/download/i103845/blackvariant/button-ui-requests-13/Snake.ico">
-             `;
-
+            <div class="app-container snake"><div class="snake__score-block">
+                    <p class="snake__score">Score: 0</p>
+                </div>
+                <div class="snake__buttons">
+                    <div class="snake__buttons--up">
+                        <div class="snake__button">↑</div>                        
+                    </div>
+                    
+                    <div class="snake__buttons--down">
+                        <div class="snake__button">←</div>
+                        <div class="snake__button">↓</div>
+                        <div class="snake__button">→</div>                        
+                    </div>
+                </div>
+                </div>`;
         // Встраиваем этот HTML в экран
         this.loadComponent(screen);
 
-
+        var currentOperation = '';
+        var snakeButtons = document.getElementsByClassName('snake__button');
+        var score = document.getElementsByClassName('snake__score')[0];
+        for (var i=0; i< snakeButtons.length; i++) {
+            var button = snakeButtons[i];
+            // IF DIGIT
+            if ( !isNaN(button.innerHTML-1)) {
+                button.onclick = function (event) {
+                    if (score.innerText === 0) {
+                        score.innerText = event.target.innerHTML;
+                    } else {
+                        score.innerText += event.target.innerHTML;
+                    }
+                }
+            } else if (button.innerText === '←') {
+                button.onclick = function (event) {
+                    score.innerText = 'left';
+                    currentOperation = '';
+                }
+            } else if (button.innerText === '→') {
+                button.onclick = function () {
+                    currentOperation = '→'
+                    score.innerHTML = 'right';
+                }
+            } else if (button.innerText === '↓') {
+                button.onclick = function (event) {
+                    score.innerText = 'down';
+                    currentOperation = '↓';
+                }
+            } else if (button.innerText === '↑') {
+                button.onclick = function () {
+                    currentOperation = '↑'
+                    score.innerHTML = 'up';
+                }
+            }
+        }
         // Определяем логику (тут код короч сам)
         
     }
